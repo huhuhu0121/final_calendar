8acafddac9b5d48fa1af96e1877eb304b2f22c0c
davong09 <davin0910@naver.com>
2024-08-12 15:00:06 +0900

    실행 시키면 초기 화면 로그인 화면으로 뜨게 함

---
+++ src/main/java/com/javalab/calendar/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.javalab.calendar.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                        .requestMatchers("/", "/index.html", "/css/**", "/js/**").permitAll()  // 인증 없이 접근 허용
+                        .anyRequest().authenticated()  // 그 외 모든 요청은 인증 필요
+                )
+                .formLogin(formLogin -> formLogin
+                        .loginPage("/login2")  // 커스텀 로그인 페이지 설정
+                        .permitAll()  // 로그인 페이지는 인증 없이 접근 허용
+                )
+                .logout(logout -> logout
+                        .permitAll()
+                );
+
+        return http.build();
+    }
+}
